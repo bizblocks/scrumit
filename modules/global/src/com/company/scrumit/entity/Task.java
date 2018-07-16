76ac6ac7559fbe2b5b21f77647afede4ec75694a
a.sokolov <asokolov@groupstp.ru>
2018-07-16 16:54:57 +0800

    Форма списка задач

--- modules/global/src/com/company/scrumit/entity/Task.java
+++ modules/global/src/com/company/scrumit/entity/Task.java
@@ -1,25 +1,16 @@
 package com.company.scrumit.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
-import com.haulmont.cuba.core.entity.StandardEntity;
-import javax.persistence.Column;
-import javax.validation.constraints.NotNull;
-import com.haulmont.chile.core.annotations.NamePattern;
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
-import java.util.Date;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 import com.haulmont.chile.core.annotations.MetaProperty;
-import javax.persistence.Transient;
-import java.util.List;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
+import com.haulmont.chile.core.annotations.NamePattern;
+import com.haulmont.cuba.core.entity.StandardEntity;
 import com.haulmont.cuba.core.entity.annotation.Listeners;
-import javax.persistence.Lob;
+import com.haulmont.cuba.core.entity.annotation.Lookup;
+import com.haulmont.cuba.core.entity.annotation.LookupType;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+import java.util.List;
 
 @Listeners("scrumit_TaskListener")
 @NamePattern("%s|shortdesc")
@@ -48,6 +39,7 @@ public class Task extends StandardEntity {
     @Column(name = "DESCRIPTION")
     protected String description;
 
+    @Lookup(type = LookupType.DROPDOWN, actions = {"lookup", "clear"})
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "PERFORMER_ID")
     protected Performer performer;
