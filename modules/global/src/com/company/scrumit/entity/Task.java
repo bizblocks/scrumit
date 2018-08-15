107b5b86463e068db4741e6fa1849d35fc8f54f6
a.sokolov <asokolov@groupstp.ru>
2018-08-15 12:24:46 +0800

    unneeded attribute

--- modules/global/src/com/company/scrumit/entity/Task.java
+++ modules/global/src/com/company/scrumit/entity/Task.java
@@ -10,8 +10,6 @@ import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.Date;
 import java.util.List;
-import com.haulmont.cuba.core.entity.annotation.OnDelete;
-import com.haulmont.cuba.core.global.DeletePolicy;
 
 @Listeners("scrumit_TaskListener")
 @NamePattern("%s|shortdesc")
@@ -105,18 +103,6 @@ public class Task extends StandardEntity {
     }
 
 
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "TRACKER_ID_ID")
-    protected Tracker trackerId;
-
-    public void setTrackerId(Tracker trackerId) {
-        this.trackerId = trackerId;
-    }
-
-    public Tracker getTrackerId() {
-        return trackerId;
-    }
 
 
     public void setSsId(Long ssId) {
