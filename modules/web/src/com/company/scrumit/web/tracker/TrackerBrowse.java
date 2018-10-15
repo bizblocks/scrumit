46505600cac211874660d297194a16d3897aa2be
itavgur <itavgur@gmail.com>
2018-10-15 18:16:35 +0800

    added button "Take task" in tracker form removed column "Testing plan"

--- modules/web/src/com/company/scrumit/web/tracker/TrackerBrowse.java
+++ modules/web/src/com/company/scrumit/web/tracker/TrackerBrowse.java
@@ -1,14 +1,18 @@
 package com.company.scrumit.web.tracker;
 
-import com.company.scrumit.entity.Tracker;
+import com.company.scrumit.entity.*;
 import com.company.scrumit.web.entity.UiEvent;
+import com.company.scrumit.web.task.TaskEdit;
+import com.haulmont.cuba.core.global.DataManager;
+import com.haulmont.cuba.core.global.LoadContext;
+import com.haulmont.cuba.core.global.Metadata;
+import com.haulmont.cuba.core.global.UserSessionSource;
 import com.haulmont.cuba.gui.WindowManager;
-import com.haulmont.cuba.gui.components.AbstractLookup;
-import com.haulmont.cuba.gui.components.CheckBox;
-import com.haulmont.cuba.gui.components.GroupTable;
+import com.haulmont.cuba.gui.components.*;
 import org.springframework.context.event.EventListener;
 
 import javax.inject.Inject;
+import java.util.HashMap;
 import java.util.Map;
 
 public class TrackerBrowse extends AbstractLookup {
@@ -17,6 +21,15 @@ public class TrackerBrowse extends AbstractLookup {
 
     @Inject
     private GroupTable<Tracker> trackersTable;
+
+    @Inject
+    private DataManager dataManager;
+
+    @Inject
+    private Metadata metadata;
+
+    @Inject
+    private UserSessionSource userSessionSource;
 
     /**
      * Called by the framework after creation of all components and before showing the screen.
@@ -39,4 +52,41 @@ public class TrackerBrowse extends AbstractLookup {
         }
     }
 
+    /**
+     * take selected task (assign it to current user)
+     */
+    public void onTakeTaskBtnClick() {
+        Tracker tracker = trackersTable.getSingleSelected();
+        if (tracker == null) return;
+
+        Performer currentPerfomer = dataManager.load(LoadContext.create(Performer.class).setQuery(LoadContext.createQuery(
+                "select p from scrumit$Performer p where p.id=:id")
+                .setParameter("id", userSessionSource.currentOrSubstitutedUserId())));
+
+        if (currentPerfomer == null) return;
+
+        tracker.setPerformer(currentPerfomer);
+        dataManager.commit(tracker);
+
+        if (((CheckBox) getComponent("createTask")).getValue()) {
+            createTask(currentPerfomer);
+        }
+        getDsContext().refresh();
+    }
+
+    private void createTask(Performer currentPerfomer) {
+
+        Map<String, Object> paramMap = new HashMap<>();
+        paramMap.put("perfomer", currentPerfomer);
+
+        Task task = metadata.create(Task.class);
+
+        TaskEdit editor = (TaskEdit) openEditor(task, WindowManager.OpenType.DIALOG, paramMap);
+        ((LookupField) ((FieldGroup) editor.getComponent("fieldGroup")).getField("type").getComponent()).setValue(TaskType.task);
+        ((LookupField) ((FieldGroup) editor.getComponent("fieldGroup")).getField("priority").getComponent()).setValue(Priority.Middle);
+        editor.getDialogOptions().setResizable(true);
+        editor.addCloseListener(actionId -> {
+            UiEvent.push("taskRefresh");
+        });
+    }
 }
