f8b59152be8c084958c6b344848b8101d5eb7d13
asokolov@groupstp.ru <Wrin@,queswij3>
2018-12-10 15:57:05 +0800

    Tracker's type translation

--- modules/global/src/com/company/scrumit/entity/TrackerPriorityType.java
+++ modules/global/src/com/company/scrumit/entity/TrackerPriorityType.java
@@ -11,7 +11,8 @@ public enum TrackerPriorityType implements EnumClass<String> {
     Cosmetic("cosmetic"),
     Hinder("hinder"),
     Inessential("inessential"),
-    Speed("speed");
+    Speed("speed"),
+    Current("current");
 
     private String id;
 
