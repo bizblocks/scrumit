f5e0464739e5aaf943b4000afcad36a451254677
spushkarenko <s.pushkarenko@groupstp.ru>
2019-02-21 15:25:11 +0800

    sending messages by telegram bot fix

--- modules/web/src/com/company/scrumit/web/servlet/TelegramBot.java
+++ modules/web/src/com/company/scrumit/web/servlet/TelegramBot.java
@@ -1,42 +1,54 @@
 package com.company.scrumit.web.servlet;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
 import java.io.*;
-import java.net.URL;
-import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TelegramBot {
 
     private final String token;
 
+    public static final String CHATID_FIELD = "chat_id";
+    public static final String TEXT_FIELD = "text";
+
     public TelegramBot(String token){
         this.token = token;
     }
 
-
     public void sendMessage(String chatId, String text) {
-        String urlString = "https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s";
-        text = text.replace(" ", "+");
-        text = text.replace("\n", "%0A");
-        urlString = String.format(urlString, token, chatId, text);
-        performUrlString(urlString);
-    }
+        String urlString = "https://api.telegram.org/bot%s/sendMessage";
+        urlString = String.format(urlString, token);
 
-    private String performUrlString(String urlString){
+        String url = urlString;
+        HttpPost httppost = new HttpPost(url);
+
+        List<NameValuePair> params = new ArrayList<>();
+        params.add(new BasicNameValuePair(CHATID_FIELD, chatId));
+        params.add(new BasicNameValuePair(TEXT_FIELD, text));
+
+        UrlEncodedFormEntity ent = null;
         try {
-            URL url = new URL(urlString);
-            URLConnection conn = url.openConnection();
-            StringBuilder sb = new StringBuilder();
-            InputStream is = new BufferedInputStream(conn.getInputStream());
-            BufferedReader br = new BufferedReader(new InputStreamReader(is));
-            String inputLine = "";
-            while ((inputLine = br.readLine()) != null) {
-                sb.append(inputLine);
-            }
-            String response = sb.toString();
-            return response;
-        }catch (IOException e) {
+            ent = new UrlEncodedFormEntity(params, "UTF-8");
+            httppost.setEntity(ent);
+            HttpClient client = new DefaultHttpClient();
+            HttpResponse responsePOST = client.execute(httppost);
+            System.out.println(responsePOST);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        return urlString;
+
     }
 }
