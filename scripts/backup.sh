8e810abb19b7010e0b6937ac15f14c7754b58fca
asokolov@groupstp.ru <Wrin@,queswij3>
2019-02-21 09:26:57 +0800

    scripts

---
+++ scripts/backup.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ "$1"=="" ]; then
+    echo  "Usage: $0 projectName"
+    exit
+fi
+
+
+projname=$1
+today=$(date +%Y%m%d)
+
+cd $projname
+./dumpdb.sh
+cd ..
+
+mkdir backup/$today
+echo Copying project, see log 
+cp -r $projname/ backup/$today 1>> backup/$today.log
+
+echo Arc project
+tar -c --xz -C backup/ -f backup/$today.tar.xz $today/ 1>> backup/$tday.log
