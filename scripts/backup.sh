b47ba30c805637c9fde7e4fb2bb008d0745bf148
asokolov@groupstp.ru <Wrin@,queswij3>
2019-02-25 11:33:51 +0800

    backup script

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,21 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ -z "$1" ]; then
-    echo  "Usage: $0 projectName"
-    exit
-fi
-
-
-projname=$1
+projname=rtneo
 today=$(date +%Y%m%d)
 
-cd $projname
-./dumpdb.sh
-cd ..
+scripts/dumpdb.sh
+pushd ..
 
 mkdir backup/$today
 echo Copying project, see log 
 cp -r $projname/ backup/$today 1>> backup/$today.log
 
 echo Arc project
-tar -c --xz -C backup/ -f backup/$today.tar.xz $today/ 1>> backup/$tday.log
+tar -c --xz -C backup/ -f backup/$today.tar.xz $today/ 1>> backup/$today.log
+
+popd
